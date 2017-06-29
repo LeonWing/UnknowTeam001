28f62f9c3a36181244ab314d0fde3bca8d5a85b6
LeonWing <weiwenlannds@163.com>
2017-06-29 15:21:14 +0800

    Signed-off-by: LeonWing <weiwenlannds@163.com>

---
+++ Assets/Scripts/PaperPlaneBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperPlaneBehaviour : MonoBehaviour {
+
+    public float RotateSpeed = 200.0f;
+    private float RotateValue;
+    // Use this for initialization
+    void Start ()
+    {
+        //飞机初始位置
+        transform.localEulerAngles = new Vector3(230, 0, 270);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //飞机转向
+        RotateValue = transform.localEulerAngles.z - Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Mathf.Clamp(RotateValue, 30, 150) );
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public int CurrentScore = 0;
+    private Text TextScore = null;
+
+    void Start ()
+    {
+        TextScore = gameObject.GetComponent<Text>();
+        TextScore.text = CurrentScore.ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
